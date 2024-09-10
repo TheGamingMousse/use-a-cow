7f7db41d104872cc6177636cd5801ce03aa7821c
TheGamingMousse <68484800+TheGamingMousse@users.noreply.github.com>
2024-09-09 19:56:55 -0700

    Update SegtreeRecursive.cpp

--- Implementation/SegtreeRecursive.cpp
+++ Implementation/SegtreeRecursive.cpp
@@ -2,52 +2,35 @@
 using namespace std;
 using ll = long long;
 
-/**
- * How to use: set the ID and LZ_ID accordingly.
- * Also, write the apply function accordingly. 
- * That's basically it! You might need to write some
- * diff code for the build and segtree walking though, depending
- * on your use case.
- */
-// T = tree node type, which will be long long
-// U = update type, which will be array<ll, 2>
-template<class T, class U> struct LazySegtree {
-    static constexpr T ID = 0;         // neutral tree node value
-    static constexpr U LZ_ID = {0, 0}; // neutral lazy update value
+template <class Info, class Tag> 
+class LazySegtree {
+  private:
     const int n;
-    vector<T> t;
-    vector<U> lz;
- 
-    inline T comb(T a, T b) { return a + b; }
-    LazySegtree(int n) : sz(n), t(4 * sz, ID), lz(4 * sz, LZ_ID) {}
-    LazySegtree(const vector<T> &a)
-        : n((int) a.size()), t(4 * n, ID), lz(4 * n, LZ_ID) {
-        build(1, 0, n - 1, a);
-    }   
-    void build(int v, int l, int r, const vector<T> &a) {
+    vector<Info> t;
+    vector<Tag> lz;
+       
+    void build(int v, int l, int r, const vector<Info> &a) {
         if (l == r) {
             t[v] = a[l];
         } else {
             int m = (l + r) >> 1;
             build(2 * v, l, m, a);
             build(2 * v + 1, m + 1, r, a);
-            t[v] = comb(t[2 * v], t[2 * v + 1]);
+            t[v] = t[2 * v] + t[2 * v + 1];
         }
     }
-    void apply(int v, int len, U x) {
-        // applies lazy update to v, 
-        // sets lazy update to lz[v]
+    void apply(int v, int len, const Tag &x) {
+        t[v].apply(x, len);
+        lz[v].apply(x, len);
     }
     void pushdown(int v, int l, int r) {
-        if (lz[v] != LZ_ID && l != r) {
-            int m = (l + r) >> 1;
-            apply(2 * v, m - l + 1, lz[v]);
-            apply(2 * v + 1, r - m, lz[v]);
-        }
-        lz[v] = LZ_ID;
+        int m = (l + r) >> 1;
+        apply(2 * v, m - l + 1, lz[v]);
+        apply(2 * v + 1, r - m, lz[v]);
+        lz[v] = Tag();
     }
-    void upd(int v, int l, int r, int ql, int qr, U x) {
-        if (qr < l || ql > r) return;
+    void upd(int v, int l, int r, int ql, int qr, const Tag &x) {
+        if (qr < l || ql > r) { return; }
         if (ql <= l && r <= qr) {
             apply(v, r - l + 1, x);
         } else {
@@ -55,37 +38,76 @@ template<class T, class U> struct LazySegtree {
             int m = (l + r) >> 1;
             upd(2 * v, l, m, ql, qr, x);
             upd(2 * v + 1, m + 1, r, ql, qr, x);
-            t[v] = comb(t[2 * v], t[2 * v + 1]);
+            t[v] = t[2 * v] + t[2 * v + 1];
         }
     }
-    void upd(int ql, int qr, U x) {
-        upd(1, 0, n - 1, ql, qr, x);
-    }
-    T qry(int v, int l, int r, int ql, int qr) {
-        if (qr < l || ql > r) return ID;
+    
+    Info qry(int v, int l, int r, int ql, int qr) {
+        if (qr < l || ql > r) return Info();
         if (l >= ql && r <= qr) return t[v];
         pushdown(v, l, r);
         int m = (l + r) >> 1;
-        return comb(qry(2 * v, l, m, ql, qr), 
-                    qry(2 * v + 1, m + 1, r, ql, qr));
+        return qry(2 * v, l, m, ql, qr) + qry(2 * v + 1, m + 1, r, ql, qr);
     }
-    T qry(int ql, int qr) {
-        return qry(1, 0, n - 1, ql, qr);
-    }
+
     template<class F> 
-    int walk(int v, int l, int r, T cur, const F &func) {
+    int walk(int v, int l, int r, Info cur, const F &func) {
         if (l == r) {
             return l;
         } else {
             int m = (l + r) / 2;
             pushdown(v, l, r);
-            T x = comb(cur, t[2 * v]);
+            Info x = cur + t[2 * v];
             return func(x) ? walk(2 * v, l, m, cur, func) 
                            : walk(2 * v + 1, m + 1, r, x, func);
         }
     }
+
+  public:
+    LazySegtree(int n) : n(n) {
+        t.assign(4 << __lg(n), Info());
+        lz.assign(4 << __lg(n), Tag());
+    }
+
+    LazySegtree(const vector<Info> &a) : n(a.size()) {
+        t.assign(4 << __lg(n), Info());
+        lz.assign(4 << __lg(n), Tag());
+        build(1, 0, n - 1, a);
+    }
+
+    void upd(int ql, int qr, const Tag &x) {
+        upd(1, 0, n - 1, ql, qr, x);
+    }
+
+    Info qry(int ql, int qr) {
+        return qry(1, 0, n - 1, ql, qr);
+    }
+
     template<class F>
     int walk(const F &func) {
-        return walk(1, 0, n - 1, ID, func);
+        return walk(1, 0, n - 1, Info(), func);
     }
 };
+
+struct Tag {
+    // tree values, and also set the default values
+
+    /** apply this new tag to this lazy tag */
+    void apply(const Tag &t, int len) {
+        
+    }
+};
+
+struct Info {
+    // lazy tag values, and also set the default tag values 
+    
+    /** apply the lazy tag to this tree value */
+    void apply(const Tag &t, int len) {
+        
+    }
+};
+
+/** @return result of joining a amd b together */
+Info operator+(const Info &a, const Info &b) {
+
+}
