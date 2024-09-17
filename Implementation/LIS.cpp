54b1948757ae2c9b5a392abdaa15e3e1f1c8d498
Justin Ji <68484800+TheGamingMousse@users.noreply.github.com>
2024-09-17 12:43:05 -0700

    Update LIS.cpp

--- Implementation/LIS.cpp
+++ Implementation/LIS.cpp
@@ -4,10 +4,10 @@ using ll = long long;
 
 template<class T> int lis(const vector<T> &a) {
     vector<T> dp;
-	for (T i : a) {
-		int pos = lower_bound(begin(dp), end(dp), i) - begin(dp);
+    for (T i : a) {
+	int pos = lower_bound(begin(dp), end(dp), i) - begin(dp);
         if (pos == dp.size()) { dp.push_back(i); } 
         else { dp[pos] = i; }
-	}
-	return (int) dp.size();
+    }
+    return (int) dp.size();
 }
